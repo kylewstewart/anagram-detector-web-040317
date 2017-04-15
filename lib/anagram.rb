5577b041b228ff63a5733adfedff3838e9cd6855
kylewstewart <kyle.w.stewart@mac.com>
2017-04-15 16:19:22 -0400

    Completed anagram-detector-web-040317

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(matches)
+    letters = word.strip.split("")
+    words = letters.permutation(letters.size).collect {|array| array.join("")}
+    words.delete(self.word)
+    matches.select do |match_word|
+      words.include?(match_word)
+    end
+  end
+
+end
